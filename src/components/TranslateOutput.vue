be4c4c7535c71c5605982b376689898b12a7fabb
zine-fj <747810974@qq.com>
2019-04-10 20:01:10 +0800

    翻译

---
+++ src/components/TranslateOutput.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="tranlate-output">
+        <h2>{{translatedText}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tranlate-output',
+    props: {
+        translatedText: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style>
+    .tranlate-output {
+        margin-top: 30px;
+        font-size: 24px;
+    }
+</style>
